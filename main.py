39cdb065212c87900742965937990fee1ad11918
Ashutosh Pandey <2000124320@hexaware.com>
2025-11-01 16:51:18 +0530

    Update main.py: use  with_structured_output in output handling

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor, create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+#from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
@@ -15,8 +15,9 @@ from schemas import AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
+structured_llm = llm.with_structured_output(AgentResponse) # creates a aversion of the model which outputs structured data according to the Pydantic model's AgentResponse schema
 react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+#output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instruction = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=[
@@ -26,18 +27,23 @@ react_prompt_with_format_instruction = PromptTemplate(
         "agent_scratchpad",
     ]
 ).partial(
-    format_instructions=output_parser.get_format_instructions()
+    format_instructions=""
 )
 
-agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt_with_format_instruction)
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instruction
+    )
 
 agent_executor = AgentExecutor.from_agent_and_tools(
     agent=agent, tools=tools, verbose=True
 )
 
 extract_output = RunnableLambda(lambda x: x["output"]) # parsing the output key (dict) from the agent executor's result as string
-parse_output = RunnableLambda(lambda x: output_parser.parse(x)) # parsing the output string into the Pydantic model
-chain = agent_executor | extract_output | parse_output # this is langchain expression language syntax
+#parse_output = RunnableLambda(lambda x: output_parser.parse(x)) # parsing the output string into the Pydantic model
+#chain = agent_executor | extract_output | parse_output # this is langchain expression language syntax
+chain = agent_executor | extract_output | structured_llm # this is langchain expression language syntax
 
 def main():
     #print("Hello from search-agent!")
